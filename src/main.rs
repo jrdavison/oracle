3030cf274d5c72f5a68d8c72c9d03aecf896c4fa
jrdavison <jmsrdvsn@gmail.com>
2024-10-23 22:15:43 -0400

    back to slint

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,24 @@
-use druid::widget::Button;
-use druid::widget::Label;
-use druid::widget::LensWrap;
-use druid::widget::List;
-use druid::widget::TextBox;
-use druid::widget::{Container, Flex, Split};
-use druid::Color;
-use druid::{AppLauncher, Widget, WindowDesc};
-use druid::{Data, Lens};
-use im::vector;
-use im::Vector;
+// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[derive(Clone, Data, Lens, PartialEq)]
-struct TodoList {
-    items: [String; 64],
-    next_item: String,
-}
+mod position;
+mod utils;
 
-fn build_ui() -> impl Widget<TodoList> {
-    Split::columns(
-        Container::new(
-            // Dynamic list of Widgets
-            List::new(|| Label::dynamic(|data, _| format!("List item: {data}"))),
-        )
-        .border(Color::grey(0.6), 2.0),
-        Container::new(
-            Flex::column()
-                .with_flex_child(Label::new("Button placeholder"), 1.0)
-                .with_flex_child(Label::new("Textbox placeholder"), 1.0),
-        )
-        .border(Color::grey(0.6), 2.0),
-    )
-}
+use position::Position;
+use std::error::Error;
 
-fn main() {
-    let main_window = WindowDesc::new(build_ui())
-        .window_size((600.0, 400.0))
-        .title("My first Druid App");
-    let initial_data = TodoList {
-        items: vector![
-            "first item".into(),
-            "second item".into(),
-            "third item".into(),
-            "foo".into(),
-            "bar".into(),
-        ],
-        next_item: String::new(),
-    };
+slint::include_modules!();
 
-    AppLauncher::with_window(main_window)
-        .launch(initial_data)
-        .expect("Failed to launch application");
+fn main() -> Result<(), Box<dyn Error>> {
+    let ui = AppWindow::new()?;
+
+    // let pos = Position::new("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    // let mut board = [0; 64];
+    // for i in 0..64 {
+    //     board[i] = pos.get_board()[i] as i32;
+    // }
+
+    ui.run()?;
+
+    Ok(())
 }
