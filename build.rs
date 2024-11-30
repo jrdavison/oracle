7759553f4f37f71790ab105c1b910327211c15a7
jrdavison <jmsrdvsn@gmail.com>
2024-11-30 17:21:17 -0500

    wip: layout improvements

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
 fn main() {
-    slint_build::compile("ui/main.slint").expect("Slint build failed");
+    let config = slint_build::CompilerConfiguration::new().with_style("cosmic".into());
+    slint_build::compile_with_config("ui/main.slint", config).expect("Slint build failed");
 }
